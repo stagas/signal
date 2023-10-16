539f4da384693a7175776406119c7c0f929a11a9
stagas <gstagas@gmail.com>
2023-10-16 16:30:10 +0300

    save

--- src/signal.ts
+++ src/signal.ts
@@ -375,7 +375,7 @@ export function signal<T extends Ctor>(ctor: Ctor): T {
   return class extends ctor {
     constructor(...args: any[]) {
       super(...args)
-      s$(this)
+      // s$(this)
     }
   } as T
 }
