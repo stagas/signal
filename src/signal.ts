160ccea661b1744dfca76707c71e211d0c69673b
stagas <gstagas@gmail.com>
2023-10-16 17:14:54 +0300

    save

--- src/signal.ts
+++ src/signal.ts
@@ -408,7 +408,7 @@ export function reactive<T extends Ctor>(ctor: Ctor): T {
   return class extends ctor {
     constructor(...args: any[]) {
       super(...args)
-      s$(this)
+      if (new.target === this.constructor) s$(this)
     }
   } as T
 }
