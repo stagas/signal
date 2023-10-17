83accaa9befedf8c4b8f5021044d4e201a34d5b2
stagas <gstagas@gmail.com>
2023-10-18 01:04:46 +0300

    save

--- src/signal-core.ts
+++ src/signal-core.ts
@@ -868,7 +868,7 @@ export function untrack(callback?: () => any) {
 export const flush = endBatch.bind(null, true)
 
 export function of<T extends object>(of: T): NonNull<T> {
-  console.warn(pos)
+  console.warn(pos, pos === EFFECT)
   if (pos === EFFECT && evalContext) {
     return requiredFast(of)
   }
