303afc8f190e18cbe11d8a1395fb8bbb81b7e633
stagas <gstagas@gmail.com>
2023-10-18 21:54:03 +0300

    save

--- src/signal.ts
+++ src/signal.ts
@@ -553,7 +553,7 @@ export function test_signal() {
           const { x, y } = $.of(this)
           runs++
         }
-        @fn update = () => {
+        @fn update () {
           console.log($.batchDepth)
           this.x++
           this.y++
