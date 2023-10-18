7da1e4160475e5122325b0ea1c233953079c96c7
stagas <gstagas@gmail.com>
2023-10-18 10:25:39 +0300

    save

--- src/signal-core.ts
+++ src/signal-core.ts
@@ -129,7 +129,7 @@ let evalContext: Computed | Effect | undefined = undefined;
 let untrackedDepth = 0;
 
 export function untracked<T>(callback: () => T): T {
-  console.log(untrackedDepth)
+  console.log('DEPTH', untrackedDepth)
   if (untrackedDepth > 0) {
     return callback();
   }
