87c3718015de71543cc709315f60b5f87a44c730
stagas <gstagas@gmail.com>
2023-10-19 20:43:20 +0300

    save

--- src/signal-core.ts
+++ src/signal-core.ts
@@ -770,6 +770,7 @@ function endEffect(this: Effect, prevContext?: Computed | Effect) {
   if (evalContext !== this) {
     evalContext = ignored.pop();
     if (evalContext !== this) {
+      console.log(this)
       throw new Error("Out-of-order effect");
     }
   }
